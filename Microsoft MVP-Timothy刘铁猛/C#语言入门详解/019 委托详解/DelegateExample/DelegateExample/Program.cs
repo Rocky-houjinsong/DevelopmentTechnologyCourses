69b02b41dc16f1816d8d89426d0978070fdc6f0f
宋厚锦 <rocky.houjinsong@outlook.com>
2023-07-28 07:56:19 +0800

    自定义委托

---
+++ Microsoft MVP-Timothy刘铁猛/C#语言入门详解/019 委托详解/DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+        Console.WriteLine("直接调用");
+        calculator.Report();
+        Console.WriteLine("间接调用");
+        Action action = new Action(calculator.Report);
+        action.Invoke();      // C# 官方版本的调用委托格式
+        action();            // C/C++ 函数指针的调用委托方式
+
+
+        Func<int,int,int> func1 = new Func<int, int, int>(calculator.Add);
+        Func<int, int,int> func2 = new Func<int, int, int>(calculator.Sub);
+
+        int x = 100;
+        int y = 200;
+        int z = 0;
+
+        z = func1.Invoke(x, y);
+        Console.WriteLine(z);
+
+        z = func2.Invoke(x, y);
+        Console.WriteLine(z);
+    }
+}
+
+class Calculator
+{
+    public void Report() { Console.WriteLine("I Have 3 Methods"); }
+
+    public int Add(int a, int b) { return a + b; }
+
+    public int Sub(int a, int b) { return a - b; }
+}
+
